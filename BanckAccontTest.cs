e3bf66aeff84c5d79dd30a7b06755ace2aa81343
Gabriel Leopoldino da Silva <79914251+Leopoldino005@users.noreply.github.com>
2025-10-22 21:11:39 -0300

    Add files via upload

---
+++ BanckAccontTest.cs
@@ -0,0 +1,22 @@
+﻿using Bank;
+
+namespace Test
+{
+    [TestClass]
+    public sealed class BankAccountTest
+    {
+        [TestMethod]
+        public void ChargeTest()
+        {
+            Customer customer1 = new Customer("Gabriel", "123456677", "12345678971", 2005);
+            BankAccount account1 = new BankAccount(customer1, 1234, 1000);
+            double valorDeposito = 500;
+            double valordeposito = 1500;
+
+            account1.Charge(valorDeposito);
+
+            Assert.AreEqual(saldoEsperado, account1.Balance);
+
+        }
+    }
+}
